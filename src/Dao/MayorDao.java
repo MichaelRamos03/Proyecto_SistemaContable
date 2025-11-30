a452c52e0688ec3804c098a2f00854da91b144f2
BryanCortez04 <bryalex.cortez.10@gmail.com>
2025-11-29 21:56:29 -0600

    Uso de fechas en mayor
    a

--- src/Dao/MayorDao.java
+++ src/Dao/MayorDao.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Date; // Usamos java.sql.Date
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
